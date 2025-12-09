a672c1b80547f6b20a4f1540e853f86b13bfb114
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-12-09 08:41:39 +0100

    Glitchtip logging integration

--- oarepo_glitchtip/initialize.py
+++ oarepo_glitchtip/initialize.py
@@ -16,10 +16,15 @@ initialize_glitchtip()
 <rest of file>
 """
 
+import logging
 import os
 
 import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 
 def initialize_glitchtip(
@@ -38,12 +43,21 @@ def initialize_glitchtip(
 
     sentry_sdk.init(
         dsn=dsn,
-        integrations=[FlaskIntegration()],
+        integrations=[
+            FlaskIntegration(),
+            CeleryIntegration(),
+            SqlalchemyIntegration(),
+            RedisIntegration(),
+            LoggingIntegration(level=logging.INFO, event_level=logging.ERROR),
+        ],
         # send details about current user. Note: glitchtip should be run on-premises
         # and we need to remove these records after 12-18 months to comply with CESNET
         # data retention policy
+        enable_logs=True,
         send_default_pii=True,
-        traces_sample_rate=float(os.environ.get("INVENIO_GLITCHTIP_SAMPLE_RATE", "0.5")),
+        traces_sample_rate=float(
+            os.environ.get("INVENIO_GLITCHTIP_SAMPLE_RATE", "0.5")
+        ),
     )
 
     if deployment_version is None:
