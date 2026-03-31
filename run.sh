b4b6f4a36e93579fa331a4ba271a9a37186c1add
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-03-31 16:27:17 +0200

    Fake test runner (#5)

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "TODO: need to migrate oarepo-glitchtip to the new library builder !!!"
+
+if [ "$1" == "test" ]; then
+    echo "Running tests..."
+    ./run-tests.sh
+fi
