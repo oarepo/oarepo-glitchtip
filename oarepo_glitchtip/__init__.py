5df33e43accf9fc0a4bd4e24cea5ff1a348f235e
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-09-16 09:01:15 +0200

    feat: rdm14 refactor

--- oarepo_glitchtip/__init__.py
+++ oarepo_glitchtip/__init__.py
@@ -1,13 +1,11 @@
-#
-# Copyright (C) 2024 CESNET z.s.p.o.
-#
-# oarepo-glitchtip is free software; you can redistribute it and/or
-# modify it under the terms of the MIT License; see LICENSE file for more
-# details.
-#
+# SPDX-FileCopyrightText: 2024 CESNET z.s.p.o
+# SPDX-License-Identifier: MIT
+
 """Glitchtip integration for CESNET invenio flavour."""
+
+from __future__ import annotations
 
 from .ext import OARepoGlitchtipExt
 from .initialize import initialize_glitchtip
 
-__all__ = ("initialize_glitchtip", "OARepoGlitchtipExt")
+__all__ = ("OARepoGlitchtipExt", "initialize_glitchtip")
